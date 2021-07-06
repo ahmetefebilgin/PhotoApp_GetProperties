480c10874323958186b32fbf3ff63f543483e81c
Ahmet Efe Bilgin <ahmetefe98@gmail.com>
2021-07-06 16:42:11 +0300

    Photo App to Get Properties like name,size,dimension

---
+++ PhotoAppDemo/PhotoApp/PhotoApp/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace PhotoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxFoto = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbx4 = new System.Windows.Forms.TextBox();
+            this.tbx3 = new System.Windows.Forms.TextBox();
+            this.tbx2 = new System.Windows.Forms.TextBox();
+            this.tbx1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbxFoto
+            // 
+            this.pbxFoto.Location = new System.Drawing.Point(76, 28);
+            this.pbxFoto.Name = "pbxFoto";
+            this.pbxFoto.Size = new System.Drawing.Size(250, 249);
+            this.pbxFoto.TabIndex = 0;
+            this.pbxFoto.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Fotoğraf Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "İsim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Genişlik";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Yükseklik";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Boyut";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbx4);
+            this.groupBox1.Controls.Add(this.tbx3);
+            this.groupBox1.Controls.Add(this.tbx2);
+            this.groupBox1.Controls.Add(this.tbx1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(572, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(448, 274);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Özellikler";
+            // 
+            // tbx4
+            // 
+            this.tbx4.Location = new System.Drawing.Point(177, 180);
+            this.tbx4.Name = "tbx4";
+            this.tbx4.Size = new System.Drawing.Size(100, 22);
+            this.tbx4.TabIndex = 10;
+            // 
+            // tbx3
+            // 
+            this.tbx3.Location = new System.Drawing.Point(177, 127);
+            this.tbx3.Name = "tbx3";
+            this.tbx3.Size = new System.Drawing.Size(100, 22);
+            this.tbx3.TabIndex = 9;
+            // 
+            // tbx2
+            // 
+            this.tbx2.Location = new System.Drawing.Point(177, 79);
+            this.tbx2.Name = "tbx2";
+            this.tbx2.Size = new System.Drawing.Size(100, 22);
+            this.tbx2.TabIndex = 8;
+            // 
+            // tbx1
+            // 
+            this.tbx1.Location = new System.Drawing.Point(177, 41);
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(100, 22);
+            this.tbx1.TabIndex = 7;
+            this.tbx1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 540);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pbxFoto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFoto)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxFoto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbx4;
+        private System.Windows.Forms.TextBox tbx3;
+        private System.Windows.Forms.TextBox tbx2;
+        private System.Windows.Forms.TextBox tbx1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ PhotoAppDemo/PhotoApp/PhotoApp/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            pbxFoto.SizeMode = PictureBoxSizeMode.StretchImage;
+            openFileDialog1.ShowDialog();
+            pbxFoto.ImageLocation = openFileDialog1.FileName;
+
+            tbx1.Text = openFileDialog1.SafeFileName;
+            //tbx2.Text=openFileDialog1.
+            //tbx3.Text=
+            //tbx4.Text=
+
+            try
+            {
+                string imagePath = pbxFoto.ImageLocation;
+                using (var fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    using (var image = Image.FromStream(fileStream, false, false))
+                    {
+                        var height = image.Height;
+                        var width = image.Width;
+                        tbx2.Text = width.ToString();
+                        tbx3.Text = height.ToString();
+                        var size = fileStream.Length.ToString();
+
+                        tbx4.Text = (size + " byte");
+                        var tip = fileStream.GetType();
+
+                        //var size = image.Size;
+
+                        //tbx4.Text = size;
+                    }
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen bir resim dosyası seçin");
+
+            }
+
+            
+           
+           
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
